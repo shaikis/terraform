7df897cab5ce4eabdbcbf4f85aecaef2794cd939
shaikis <shaik.urs@gmail.com>
2021-08-19 21:05:02 +0530

    added tgw

---
+++ transit_gateway/transit_gw.tf
@@ -0,0 +1,19 @@
+resource "aws_ec2_transit_gateway" "tgw" {
+  count = var.create_tgw ? 1 : 0
+
+  description                     = coalesce(var.description, var.name)
+  amazon_side_asn                 = var.amazon_side_asn
+  default_route_table_association = var.enable_default_route_table_association ? "enable" : "disable"
+  default_route_table_propagation = var.enable_default_route_table_propagation ? "enable" : "disable"
+  auto_accept_shared_attachments  = var.enable_auto_accept_shared_attachments ? "enable" : "disable"
+  vpn_ecmp_support                = var.enable_vpn_ecmp_support ? "enable" : "disable"
+  dns_support                     = var.enable_dns_support ? "enable" : "disable"
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.tgw_tags,
+  )
+}
